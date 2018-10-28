ab698c1fea198c938af0c8cf683bf1a819936b73
Ediko Dominike <ediko.dominike@gmail.com>
2018-10-28 20:41:40 -0300

    Remove build(docs)

--- src/router.ts
+++ src/router.ts
@@ -6,8 +6,7 @@ Vue.use(Router);
 
 export default new Router({
   mode: 'history',
-  // base: process.env.BASE_URL '/alelotorio/',
-  base: '/alelotorio/',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
