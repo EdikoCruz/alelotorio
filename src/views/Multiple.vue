0fd404cd47324c84def118e22b375d74c635c6a2
Ediko Dominike <ediko.dominike@gmail.com>
2018-11-22 21:58:26 -0300

    Finish repetitions

--- src/views/Multiple.vue
+++ src/views/Multiple.vue
@@ -1,7 +1,77 @@
 <template>
   <div class="home container">
-    {{population.alleles}}
-    <button class="waves-effect waves-light btn" @click="generateOffspring">Gerar Gráficos</button>
+    <div class="row vertical-margin">
+      <div class="col s12 l6">
+        <div class="card horizontal">
+          <div class="card-stacked">
+            <div class="card-content">
+              <p class="center-align title"><b>{{population.name}}</b></p>
+              <p class="center-align"><b>Tamanho: </b>{{population.size}}</p>
+              <p class="center-align"><b>Repetições: </b>{{repetitions}}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col s12 l6">
+        <div class="card horizontal">
+          <div class="card-stacked">
+            <div class="card-content">
+              <p class="center-align">Tamanho</p>
+              <input
+                type="range"
+                :min="config.minSize"
+                :max="config.maxSize"
+                :step="config.step"
+                v-model="population.size"/>
+              <p class="center-align">Repetições</p>
+              <input
+                type="range"
+                :min="config.minNumberOfRepetions"
+                :max="config.maxNumberOfRepetions"
+                :step="config.stepNumberOfRepetions"
+                v-model="repetitions"/>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 center-align vertical-align">
+        <button
+          class="waves-effect waves-light btn"
+          @click="beforeGenerate" >
+          Gerar Gráficos
+        </button>
+      </div>
+
+      <div class="col s12 center-align vertical-margin">
+        <div class="preloader-wrapper big active" v-show="loading">
+          <div class="spinner-layer spinner-green-only">
+            <div class="circle-clipper left">
+              <div class="circle"></div>
+            </div><div class="gap-patch">
+              <div class="circle"></div>
+            </div><div class="circle-clipper right">
+              <div class="circle"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 l8">
+        <column-chart
+          v-show="!loading"
+          ytitle="Fixações"
+          xtitle="Geração"
+          :data="generationsData" />
+      </div>
+      <div class="col s12 l4">
+        <pie-chart
+          v-show="!loading"
+          :data="allelesData"
+          :colors="[config.colors.a1, config.colors.both, config.colors.a2]"
+          :donut="true" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,12 +83,16 @@ import { Component, Vue } from 'vue-property-decorator';
 
   data() {
     return {
+      loading: false,
+      repetitions: 200,
       config: {
         maxNumberOfGenerations: 10000,
+        maxNumberOfRepetions: 1000,
+        minNumberOfRepetions: 100,
+        stepNumberOfRepetions: 50,
         minSize: 4,
-        maxSize: 500,
+        maxSize: 1000,
         step: 4,
-        repetitions: 10000,
         colors: {
           a1: '#f48fb1',
           a2: '#9fa8da',
@@ -26,30 +100,39 @@ import { Component, Vue } from 'vue-property-decorator';
         },
       },
       population: {
-        name: 'População Teste',
+        name: 'Aquários',
         size: 200,
-        alleles:{
+        alleles: {
           a1: 0,
           a2: 0,
           a: 0,
         },
+        generations: {},
       },
     };
   },
 
   methods: {
+    beforeGenerate(): void {
+      // @ts-ignore
+      this.loading = true;
+      // @ts-ignore
+      setTimeout(() => this.generateOffspring(), 100);
+    },
     generateOffspring(): void {
       const that: any = this;
       // data bind
       const population: any = that.population;
       const maxNumberOfGenerations: number = that.config.maxNumberOfGenerations;
-      const repetitions: number = that.config.repetitions;
+      const repetitions: number = that.repetitions;
       const histogramMultiplicity: number = that.config.histogramMultiplicity;
       const total = population.size * 2;
       const size = population.size;
+      const factor = that.getFactor();
       const alleles = {a1: 0, a2: 0, a: 0};
+      const generations: any = {};
 
-      for (let _ = 0; _ < repetitions; _++) {
+      for (let i = 0; i < repetitions; i++) {
         let a1 = population.size;
         let a2 = population.size;
 
@@ -75,31 +158,65 @@ import { Component, Vue } from 'vue-property-decorator';
           a2 = a2Counter;
           generation += 1;
         }
-        
 
         if (a1 === total) {
-          population.alleles.a1 += 1;
-        }
-        if (a2 === total) {
-          population.alleles.a2 += 1;
-        }
-        if (generation > maxNumberOfGenerations) { // bug with else =/
-          population.alleles.a += 1;
+          alleles.a1 += 1;
+        } else if (a2 === total) {
+          alleles.a2 += 1;
+        } else {
+          alleles.a += 1;
         }
 
-        population.alleles = alleles;
+        const leanGen = Math.floor(generation / factor);
+        generations[leanGen] = generations[leanGen] || 0;
+        generations[leanGen] += 1;
       }
 
-      // population.generation = Math.min(generation, maxNumberOfGenerations);
+      population.alleles = alleles;
+      population.generations = generations;
+      // @ts-ignore
+      this.loading = false;
+    },
+    getFactor(): number {
+      const that: any = this;
+      const population: any = that.population;
+      const size: number = population.size;
+      return Math.floor(size / 4);
     },
   },
 
-  computed: {},
+  computed: {
+    allelesData(): any {
+      const that: any = this;
+      // data bind
+      const population: any = that.population;
+      return [
+        ['A1', population.alleles.a1],
+        ['Ambos', population.alleles.a],
+        ['A2', population.alleles.a2],
+      ];
+    },
+    generationsData(): any[] {
+      const that: any = this;
+      // data bind
+      const population: any = that.population;
+      const factor: any = that.getFactor();
+
+      return Object.keys(population.generations).reduce((p: any, c: any) => {
+        p.push([Number(c) * factor, population.generations[c]]);
+        return p;
+      }, []);
+    },
+  },
 })
 export default class Home extends Vue {}
 </script>
 
 <style lang="scss">
+.title {
+  font-size: 1.5em;
+  padding: 10px 0;
+}
 .vertical-margin{
   margin: 30px 0;
 }
